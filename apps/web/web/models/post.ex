e963740729b74c602ca78fd3ee16e7d5f817fa80
Ian Duggan <ian@ianduggan.net>
2016-10-06 20:07:20 -0700

    Posts example for RethinkDB

---
+++ apps/web/web/models/post.ex
@@ -0,0 +1,21 @@
+defmodule Web.Post do
+  use Web.Web, :model
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "posts" do
+    field :title, :string
+    field :content, :string
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title, :content])
+    |> validate_required([:title, :content])
+  end
+end
